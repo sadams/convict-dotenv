e0ef9d2ecd0889c76c93ccde8d1ae7f1d3bf706a
Sam Adams <adams.sam@gmail.com>
2020-09-23 14:20:10 +0100

    feat(cd): automate semantic releases and commit messages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,15 @@
 module.exports = {
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   env: {
     commonjs: true,
     es2021: true,
     node: true,
   },
-
-  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
-
   parserOptions: {
     ecmaVersion: 12,
   },
-
   plugins: ['prettier'],
   rules: {
     'prettier/prettier': 'error',
-
-    // rules: {
-    //   semi: ['error', 'always'],
-    //   'comma-dangle': ['error', 'only-multiline'],
   },
 };
